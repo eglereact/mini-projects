6d810d7d42559f5455f7110032de2971b316e4f7
eglereact <eglereactjs@gmail.com>
2022-04-14 16:56:21 +0300

    update README

--- contact-form/src/App.js
+++ contact-form/src/App.js
@@ -15,7 +15,7 @@ function App() {
         >
           <h4 className="text-white text-2xl -rotate-90 ">Contact Info</h4>
         </div>
-        <div className="bg-white h-[500px] flex flex-col rounded-lg w-full py-6 px-10">
+        <div className="bg-white h-[500px] flex flex-col rounded-lg w-full py-6 px-10 my-2">
           <h1 className="text-2xl text-gray-800 mb-6">Send us a Message</h1>
           <div className="flex flex-col items-center  w-full">
             <input
@@ -39,23 +39,20 @@ function App() {
           </p>
           <div className="flex items-center mb-6">
             <input type="radio" id="email" name="drone" value="email" />
-            <label for="email" className="mr-4 ml-1 text-lg text-gray-800">
+            <label htmlFor="email" className="mr-4 ml-1 text-lg text-gray-800">
               Email
             </label>
             <input type="radio" id="phone" name="drone" value="phone" />
-            <label for="phone" className="mr-4 ml-1 text-lg text-gray-800">
+            <label htmlFor="phone" className="mr-4 ml-1 text-lg text-gray-800">
               Phone
             </label>
           </div>
           <p className="text-lg text-gray-800">Message</p>
           <textarea className="outline-none border-b w-full border-gray-300 mb-4" />
-          {/* <button className="bg-[#FB44D2] text-white w-full rounded-full py-4 mt-6">
-            Submit
-          </button> */}
-          <button class="relative  inline-flex items-center justify-center p-4 px-5 py-5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
-            <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-            <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-            <span class="relative text-white">Button Text</span>
+          <button className="relative  inline-flex items-center justify-center p-4 px-5 py-5 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+            <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+            <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+            <span className="relative text-white">Button Text</span>
           </button>
         </div>
       </div>
